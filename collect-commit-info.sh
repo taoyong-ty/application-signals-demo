b1d784e01c4e75558f3cb461fa9a173264387405
Yong Tao <117978688+taoyong-ty@users.noreply.github.com>
2025-04-07 23:56:46 -0700

    Update collect-commit-info.sh

--- collect-commit-info.sh
+++ collect-commit-info.sh
@@ -52,7 +52,7 @@ if git rev-parse HEAD &>/dev/null; then
   COMMIT_MESSAGE=$(git show -s --format='%B' $COMMIT_ID)
   
   # Get commit diff
-  COMMIT_DIFF=$(git format-patch -1 $COMMIT_ID --stdout)
+  COMMIT_DIFF=$(git diff $COMMIT_ID~ $COMMIT_ID)
 
   # Create commit entity properties as JSON string
   COMMIT_PROPS=$(cat << EOF
