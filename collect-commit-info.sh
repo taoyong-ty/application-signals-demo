8534295099aced54cab74d8bff43b8a692a23f3c
Yong Tao <taoyong@amazon.com>
2025-04-08 09:39:43 -0700

    update diff extraction

--- collect-commit-info.sh
+++ collect-commit-info.sh
@@ -52,7 +52,7 @@ if git rev-parse HEAD &>/dev/null; then
   COMMIT_MESSAGE=$(git show -s --format='%B' $COMMIT_ID)
   
   # Get commit diff
-  COMMIT_DIFF=$(git show $COMMIT_ID --diff-merges=on) 
+  COMMIT_DIFF=$(git diff origin/main origin/${GITHUB_HEAD_REF})
 
   # Create commit entity properties as JSON string
   COMMIT_PROPS=$(cat << EOF
