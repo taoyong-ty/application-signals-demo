988f42572ba2382159796ffcce9a145c86f7345a
Yong Tao <117978688+taoyong-ty@users.noreply.github.com>
2025-04-10 22:36:21 -0700

    Update database.tf

--- terraform/eks/database.tf
+++ terraform/eks/database.tf
@@ -155,4 +155,25 @@ resource "aws_dynamodb_table" "payment_table" {
 
 }
 
+resource "aws_dynamodb_table" "global-table" {
+  name             = "global-table"
+  hash_key         = "TestTableHashKey"
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  attribute {
+    name = "TestTableHashKey"
+    type = "S"
+  }
+
+  replica {
+    region_name = "us-east-2"
+  }
+
+  replica {
+    region_name = "us-west-2"
+  }
+}
+
 
