f4671a368b40531985530c872ccbc522d813d46d
Yong Tao <117978688+taoyong-ty@users.noreply.github.com>
2025-04-17 15:28:11 -0700

    Update database.tf

--- terraform/eks/database.tf
+++ terraform/eks/database.tf
@@ -81,7 +81,9 @@ resource "aws_dynamodb_table" "billing_table" {
   #checkov:skip=CKV_AWS_119:demo only, no encryption is needed
 
   name           = "BillingInfo"
-  billing_mode   = "PAY_PER_REQUEST"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 10
+  write_capacity = 10
   hash_key       = "ownerId"
   range_key      = "timestamp"
 
